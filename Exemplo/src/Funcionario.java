d92a312b08c7dd54c25e022593ec7813987a7c61
comp8 <comp8@comp8_01.lab.unesc.net>
2017-03-31 19:54:50 -0300

    Adicionado uma obs de liberação de versão

--- Exemplo/src/Funcionario.java
+++ Exemplo/src/Funcionario.java
@@ -1,12 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author comp8
+ * Você precisa de uma licença
+ * @author Gilberto Vieira da Silva
  */
 public class Funcionario extends javax.swing.JFrame {
 
